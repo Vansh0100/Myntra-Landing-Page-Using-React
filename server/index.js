5c8677c58fd662d4c0c41f914a37ea04a53f27d1
Vansh0100 <vanshjaiswal945@gmail.com>
2022-01-19 23:24:18 +0530

    basic server setup

--- server/index.js
+++ server/index.js
@@ -1,2 +1,21 @@
 // configuring dotenv
-require('dotenv').config();
+import dotenv from "dotenv";
+dotenv.config();
+
+// Importing modules
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+// Initialising express app
+const app=express();
+
+// Using the app
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// Setting up the port
+app.listen(4000,()=>{
+    console.log("Server is running!");
+})
